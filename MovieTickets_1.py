b40a3d6ec7faa67ea476b5fc94d5dba234f7a39e
AndreaRootman <Rootmana2@middleton.school.nz>
2023-03-14 10:12:32 +1300

    Changes

--- MovieTickets_1.py
+++ MovieTickets_1.py
@@ -1,1 +1,21 @@
-print("Hello world")
+"""Movie theater ticket system -v1
+welcome screen and variables
+created by Andrea Rootman
+"""
+#component 1 - welcome screen and set up variables
+def sell_ticket():
+    print("********** potato movies - ticketing system ***********\n")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_tickets =0
+
+
+
+#main routine
+sell_ticket()
+
+
